30ed0b522bd421077f92c21119950a7551843940
cgreen14 <cgreen14@nd.edu>
2017-04-17 19:45:30 -0400

    start page and instructions

--- defense.py
+++ defense.py
@@ -26,12 +26,13 @@ level1Map = pygame.transform.scale(level1Map, (scrWidth, scrHeight))
 level1MapRect = level1Map.get_rect()
 #Set Up Object to Move, we are inserting the image, fliping it and resizing it
 tankImg = pygame.transform.rotozoom(pygame.image.load('tank1.png'),270, 0.3)
+towerImg = pygame.transform.rotozoom(pygame.image.load('tower1.png'),0, 0.3)
 
 def tankDraw(x,y):
 	gameDisplay.blit(tankImg, (x,y))
 
-x = scrWidth * 0.05
-y = scrHeight * 0.52
+def drawTower(x,y):
+	gameDisplay.blit(towerImg, (x,y))
 
 def runAlongTrack(x,y):
 	speed = 10
@@ -54,40 +55,53 @@ def runAlongTrack(x,y):
 
 
 #Main
-
+x = scrWidth * 0.05
+y = scrHeight * 0.52
 dvert = 0
 crashed = False
-while (x > 0 and x<800 and y > 0 and y < 600):
-	x,y = runAlongTrack(x,y)
-	print x
-	print y
-#	dhoz += 1
+#Board set up
+
+
+#Game start
+end_it=False
+while (end_it==False):
+    gameDisplay.fill((0,0,0))
+    myfont=pygame.font.SysFont("Britannic Bold", 40)
+    nlabel=myfont.render("Welcome: Click 's' to begin", 1, (255, 0, 0))
+    for event in pygame.event.get():
+        if event.type==pygame.KEYDOWN:
+			if event.key == pygame.K_s:
+				end_it=True
+    gameDisplay.blit(nlabel,(200,200))
+    pygame.display.flip()
 	#keep in bounds
+
+gameDisplay.fill((0,0,0))
+myfont=pygame.font.SysFont("Britannic Bold", 40)
+nlabel=myfont.render("Welcome: Click 'b' to start setting up the map", 1, (255, 0, 0))
+gameDisplay.blit(nlabel,(75,200))
+pygame.display.flip()
+while(1):
 	for event in pygame.event.get():  #next week work to update this to take into account pressed keys
 		if event.type == pygame.QUIT:
 			sys.exit()
 		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT:
-				x -= 10
-			elif event.key == pygame.K_RIGHT:
-				x += 10
-			elif event.key == pygame.K_UP:
-				y -= 10
-			elif event.key == pygame.K_DOWN:
-				y += 10
-# could possibly due mouse clicks to buttons here, to have player build into
-#board
-			elif event.key == pygame.K_g:
-				tankImg = pygame.transform.rotate(tankImg,rotate*90)
-
-	gameDisplay.fill((0, 0, 0))
-	gameDisplay.blit(level1Map, level1MapRect)
-	# this would be the place to check for valid tank movement, would need to check
-	# before making the change then revert
-	tankDraw(x,y)
-	pygame.display.update()
-	clock.tick(FPS)
-	pygame.display.flip()
-
+			if event.key == pygame.K_b:
+					gameDisplay.fill((0, 0, 0))
+					gameDisplay.blit(level1Map, level1MapRect)
+					pygame.display.update()
+					clock.tick(FPS)
+					pygame.display.flip()
+			if event.key == pygame.K_s:
+					x,y = runAlongTrack(x,y)
+					gameDisplay.fill((0, 0, 0))
+					gameDisplay.blit(level1Map, level1MapRect)
+					# this would be the place to check for valid tank movement, would need to check
+					# before making the change then revert
+					tankDraw(x,y)
+					drawTower(40,80)
+					pygame.display.update()
+					clock.tick(FPS)
+					pygame.display.flip()
 
 pygame.quit()
