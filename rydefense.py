52747508baa44018c23bd4b64d6d46b2cedce0e7
Ryan Green <rgreen8@nd.edu>
2017-04-25 13:07:59 -0400

    hope

--- rydefense.py
+++ rydefense.py
@@ -38,6 +38,7 @@ nlabelp = myfont.render("Play", 1, black)
 nlabellead = myfont.render("Leaders", 1, black)
 nlabeltower = myfont2.render("New Tower", 1, black)
 nlabeltowerc = myfont2.render("Cost: 200", 1, black)
+nlabelplay = myfont2.render("RUN", 1, black)
 
 
 def runAlongTrack(x,y):
@@ -68,6 +69,8 @@ def tankDraw(x,y, tankImg):
 		tankImg = pygame.transform.rotate(tankImg, 90)
 	gameDisplay.blit(tankImg, (x,y))
 
+#def checkBounds(x,y):
+	#return false or out of bounds if on path and true if not in path
 
 
 #Main
@@ -75,9 +78,6 @@ x = scrWidth * 0.05
 y = scrHeight * 0.52
 dvert = 0
 crashed = False
-#Board set up
-
-
 #Game start
 end_start=False
 gameQuit = False
@@ -107,23 +107,66 @@ while not end_start:
 	pygame.display.flip()
 
 #main game
-xs = scrWidth * 0.05
+xs = scrWidth * 0.02
 ys = scrHeight * 0.52
+towerc = 0
+towers = []
+carc = 0
+cars = []
 while not gameQuit:
+	#set board
+	gameDisplay.blit(level1Map, level1MapRect)
+	runbutton = pygame.draw.rect(gameDisplay, red, [scrWidth*0.05, scrHeight*0.05, scrWidth*0.075, scrHeight*0.05])
+	towerbutton = pygame.draw.rect(gameDisplay, white, [scrWidth*0.85, scrHeight*0.9, scrWidth*0.1, scrHeight*0.075])
+	gameDisplay.blit(nlabeltower, (scrWidth*0.86, scrHeight*0.91))
+	gameDisplay.blit(nlabeltowerc, (scrWidth*0.86, scrHeight*0.94))
+	gameDisplay.blit(nlabelplay, (scrWidth*0.05, scrHeight*0.05))
+	#Grab the next movement
+	pygame.display.update()
+	clock.tick(FPS)
+	placetower = False
+	runround = False
 	for event in pygame.event.get():  #next week work to update this to take into account pressed keys
 		if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
 			gameQuit = True
 		if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
 			xs, ys = runAlongTrack(xs, ys)
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			pos = pygame.mouse.get_pos()
+			(pressed1,pressed2,pressed3) = pygame.mouse.get_pressed()
+			#Place Tower Fella
+			if towerbutton.collidepoint(pos) and pressed1 == 1:
+				placetower = True
+			#Run Simulation
+			if runbutton.collidepoint(pos) and pressed1 == 1:
+				runround = True
 
-	gameDisplay.blit(level1Map, level1MapRect)
-	towerbutton = pygame.draw.rect(gameDisplay, white, [scrWidth*0.85, scrHeight*0.9, scrWidth*0.1, scrHeight*0.075])
-	gameDisplay.blit(nlabeltower, (scrWidth*0.86, scrHeight*0.91))
-	gameDisplay.blit(nlabeltowerc, (scrWidth*0.86, scrHeight*0.94))
-	tankDraw(xs,ys, tankImg)
-	pygame.display.update()
-	clock.tick(FPS)
-	pygame.display.flip()
 
+
+	if placetower: #preform actions to place a tower
+		#follow mouse to new placement
+		placed = False
+		while not placed:
+			pos = pygame.mouse.get_pos()
+			#if checkBounds(pos):
+				#display red or green cirle depending on results
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
+					placed = False
+					gameQuit = True
+				if event.type == pygame.MOUSEBUTTONDOWN:
+					placed = False
+
+
+		#draw new tower and store position
+		#gameDisplay.blit(towerImg, pos)
+		#towers.append(pos)
+
+	if runround: #run the simulation
+		print "run"
+
+
+	#for towerpos in towers:
+	#	gameDisplay.blit(towerImg, towerpos)
 
 pygame.quit()
