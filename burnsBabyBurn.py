8841500f268c67f93d83c298e7f667512b972a74
Daniel Burns <danieljburns143@gmail.com>
2017-04-28 14:38:54 -0400

    Working health bars

--- burnsBabyBurn.py
+++ burnsBabyBurn.py
@@ -21,19 +21,23 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 gameContinue = True
 
-##########################
-# Images/Image Positions #
-##########################
+##########
+# Images #
+##########
+# Images
 backgroundImg = pygame.image.load("level1Map.png")
+tank1Img = pygame.image.load("tank1.png")
+tank2Img = pygame.image.load("tank2.png")
+tank3Img = pygame.image.load("tank3.png")
+towerImg = pygame.image.load("tower1.png")
+# Transformation
 backgroundImg = pygame.transform.scale(backgroundImg, (scrWidth, scrHeight))
 backgroundImgRect = backgroundImg.get_rect()
-tank1Img = pygame.image.load("tank1.png")
 tank1Img = pygame.transform.scale(tank1Img, (tank1Img.get_width() / 3, tank1Img.get_height() / 3))
 tank1Img = pygame.transform.rotate(tank1Img, -90)
-tank2Img = pygame.image.load("tank2.png")
 tank2Img = pygame.transform.scale(tank2Img, (tank2Img.get_width() / 3, tank2Img.get_height() / 3))
 tank2Img = pygame.transform.rotate(tank2Img, -90)
-tank3Img = pygame.image.load("tank3.png")
+towerImg = pygame.transform.scale(towerImg, (towerImg.get_width() / 4, towerImg.get_height() / 4))
 
 ###########
 # Classes #
@@ -47,7 +51,7 @@ class Tank(pygame.sprite.Sprite):
 		self.rect.y = 310
 		self.velX = 1
 		self.velY = 0
-		self.heath = 100
+		self.health = 100
 		self.angle = 0
 	def update(self):
 		if self.rect.x < 110:
@@ -93,19 +97,32 @@ class Tank(pygame.sprite.Sprite):
 			self.kill()
 		self.rect.x += self.velX
 		self.rect.y += self.velY
+		pygame.draw.line(gameDisplay, red, (self.rect.x, self.rect.y), \
+			(self.rect.x + self.health * .3, self.rect.y), 2)
+		if self.health <= 0:
+			enemies.clear(gameDisplay, backgroundImg)
+			self.kill()
+
+class Tower(pygame.sprite.Sprite):
+	def __init__(self):
+		super(Tower, self).__init__()
+		self.image = towerImg
+		self.rect = self.image.get_rect()
+		self.rect.x = 735
+		self.rect.y = 194
+	def update(self):
+		pygame.draw.line(gameDisplay, red, self.rect.bottomleft, self.rect.bottomright, 2)
 
 ##########
 # Groups #
 ##########
 enemies = pygame.sprite.Group()
+towers = pygame.sprite.Group()
 
 #############
 # Test Case #
 #############
-enemy1 = Tank(tank1Img)
-enemies.add(enemy1)
-startGameTime = time.time()
-newEnemyAdded = False
+towers.add(Tower())
 
 #################
 # Initiate Game *
@@ -114,18 +131,21 @@ pygame.init()
 pygame.display.set_caption("Tower Defense")
 clock = pygame.time.Clock()
 gameDisplay = pygame.display.set_mode((scrWidth, scrHeight))
+gameDisplay.blit(backgroundImg, backgroundImgRect)
 while gameContinue:
-	if time.time() - startGameTime > 4 and newEnemyAdded == False:
-		enemy2 = Tank(tank2Img)
-		enemies.add(enemy2)
-		newEnemyAdded = True
 	clock.tick(FPS)
-	gameDisplay.blit(backgroundImg, backgroundImgRect)
-	if len(enemies.sprites()) == 0:
-		gameContinue = False
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
+		if event.type == pygame.QUIT:
 			gameContinue = False
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_q:
+				gameContinue = False
+			if event.key == pygame.K_n:
+				enemies.add(Tank(tank1Img))
+	enemies.clear(gameDisplay, backgroundImg)
 	enemies.update()
+	towers.clear(gameDisplay, backgroundImg)
+	towers.update()
 	enemies.draw(gameDisplay)
+	towers.draw(gameDisplay)
 	pygame.display.update()
